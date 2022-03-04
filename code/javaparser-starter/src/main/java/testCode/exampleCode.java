5ab2ff14a5a969ad221cd0dc647f52bc4d329ff9
Sophie Gadrey <gadreysophie@gmail.com>
2022-03-04 12:01:54 +0100

    add exampleCode.java

---
+++ code/javaparser-starter/src/main/java/testCode/exampleCode.java
@@ -0,0 +1,16 @@
+package testCode;
+
+public class exampleCode {
+
+    private String firstname;
+    private int age;
+    private String name;
+    private String adresse;
+
+    public String getName() { return name; }
+    public String getFirstname() {return firstname;}
+
+    public boolean isAdult() {
+        return age > 17;
+    }
+}
